85dc8390c422b2c8a39c2e3453ce44c7ca311177
jaceguay <jaceguay@gmail.com>
2020-09-24 00:10:43 -0300

    dados selecionados join

--- dados_selecionados/join_data.py
+++ dados_selecionados/join_data.py
@@ -2,18 +2,44 @@
 import pandas as pd
 
 # %%
-df1 = pd.read_csv('cnaes_municipios.csv')
-df2 = pd.read_csv('Domicilio01_SC.csv')
-df3 = pd.read_csv('Domicilio02_SC.csv')
-df4 = pd.read_csv('DomicilioRenda_SC.csv')
-df5 = pd.read_csv('Pessoa13_SC.csv')
-df6 = pd.read_csv('zonas_censitarias_area.csv')
+df1 = pd.read_csv('cnaes_municipios.csv', dtype={'Cod_setor': 'string'})
+df2 = pd.read_csv('Domicilio01_SC.csv', dtype={'Cod_setor': 'string'})
+df3 = pd.read_csv('Domicilio02_SC.csv', dtype={'Cod_setor': 'string'})
+df4 = pd.read_csv('DomicilioRenda_SC.csv', dtype={'Cod_setor': 'string'})
+df5 = pd.read_csv('Pessoa13_SC.csv', dtype={'Cod_setor': 'string'})
+df6 = pd.read_csv('zonas_censitarias_area.csv', dtype={'Cod_setor': 'string'})
 
 # %%
-df_lists = [df1, df2, df3, df4, df5, df6]
+df2['cod_cidade'] = df2['Cod_setor'].str.slice(0, 7)
+dfa = df2.merge(df1,
+                left_on='cod_cidade',
+                right_on='Cod_setor',
+                how='left').fillna(0)
+dfa['ncod'] = dfa['Cod_setor_x']
+# %%
+dfb = dfa.merge(df3,
+                left_on='ncod',
+                right_on='Cod_setor',
+                how='left').fillna(0)
 
 # %%
-joined = pd.concat(df_lists, join='outer', axis=1).fillna(0)
+dfc = dfb.merge(df4,
+                left_on='ncod',
+                right_on='Cod_setor',
+                how='left').fillna(0)
+
+# %%
+dfd = dfc.merge(df5,
+                left_on='ncod',
+                right_on='Cod_setor',
+                how='left').fillna(0)
+
+# %%
+joined = dfd.merge(df6,
+                   left_on='ncod',
+                   right_on='Cod_setor',
+                   how='left').fillna(0)
+# %%
 
 joined['poptotal'] = joined[['0a17',
                              '18a24',
@@ -24,7 +50,7 @@ joined['poptotal'] = joined[['0a17',
 joined['densidade'] = joined['areametros'] / joined['poptotal']
 
 # %%
-pd.DataFrame(joined[['Cod_setor',
+pd.DataFrame(joined[['ncod',
                      '41',
                      '42',
                      '43',
